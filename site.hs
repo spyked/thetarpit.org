363a5891d7071c1288a288d4671ddc699ed6b3ca
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-07-17 09:27:20 +0300

    add a bare site

--- site.hs
+++ site.hs
@@ -2,4 +2,16 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 
--- TODO: add site manually
+main :: IO ()
+main = hakyll $ do
+  match "index.html" $ do
+    route idRoute -- TODO: make a "copy to root" route?
+    compile $ do
+      let indexCtx = defaultContext
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateCompiler
